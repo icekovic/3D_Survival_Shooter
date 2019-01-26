90900dfce40509abb3711acb9167fd7ab62d3307
icekovic <icekovic@racunarstvo.hr>
2019-01-26 12:52:48 +0100

    created game canvases and second and third level

---
+++ 3D_Survival_Shooter/Assets/Scripts/Models/Scenes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenes
+{
+    public const string MainMenu = "MainMenu";
+    public const string OptionsMenu = "OptionsMenu";
+    public const string FirstLevel = "FirstLevel";
+    public const string SecondLevel = "SecondLevel";
+    public const string ThirdLevel = "ThirdLevel";
+}
