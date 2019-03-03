4785245fd039c9ed07754453e6e551d6a896558a
icekovic <icekovic@racunarstvo.hr>
2019-03-03 18:48:17 +0100

    added localized JSON texts

--- 3D_Survival_Shooter/Assets/Scripts/Localization/LocalizationManager.cs
+++ 3D_Survival_Shooter/Assets/Scripts/Localization/LocalizationManager.cs
@@ -1,18 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class LocalizationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Dictionary<string, string> localizedText;
+
+    private void Awake()
+    {
+        localizedText = new Dictionary<string, string>();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadLocalizedText(string fileName)
     {
-        
+        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+
+        if(CheckIfFilepathExists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            LocalizationData loadedData = JsonUtility.FromJson<LocalizationData> (dataAsJson);
+
+            for (int i = 0; i < loadedData.items.Length; i++)
+            {
+                localizedText.Add(loadedData.items[i].key, loadedData.items[i].value);
+            }
+
+
+            Debug.Log("Data loaded, dictionary contains: " + localizedText.Count + " entries");
+        }
+
+        else
+        {
+            Debug.LogError("Cannot find file!");
+        }
+    }
+
+    private bool CheckIfFilepathExists(string filePath)
+    {
+        if(File.Exists(filePath))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
