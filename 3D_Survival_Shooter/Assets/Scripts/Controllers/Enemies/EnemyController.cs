9eb90032f53d637e830eb69807331b5e0dc6b9b2
icekovic <icekovic@racunarstvo.hr>
2019-04-03 12:28:36 +0200

    added EnemySpawner script

---
+++ 3D_Survival_Shooter/Assets/Scripts/Controllers/Enemies/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy;
+
+    [SerializeField]
+    private float spawnTime;
+
+    [SerializeField]
+    private Transform[] spawnPoints;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void Spawn()
+    {
+        // If the player has no health left...
+        //if (playerHealth.currentHealth <= 0f)
+        //{
+        //    // ... exit the function.
+        //    return;
+        //}
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
