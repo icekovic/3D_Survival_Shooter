c9723567a7a4f2a8c78f588c5c62f3e694fa5bf5
icekovic <icekovic@racunarstvo.hr>
2019-04-24 19:19:40 +0200

    updated EnemyPoints script

--- 3D_Survival_Shooter/Assets/Scripts/Models/EnemyPoints.cs
+++ 3D_Survival_Shooter/Assets/Scripts/Models/EnemyPoints.cs
@@ -7,8 +7,4 @@ public static class EnemyPoints
     public static int zombunny = 50;
     public static int zombear = 100;
     public static int hellephant = 150;
-
-    public static int zombunniesKilled = 10;
-    public static int zombearsKilled = 20;
-    public static int hellephantsKilled = 30;
 }
